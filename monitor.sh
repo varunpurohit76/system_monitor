d1ffb434adde322c0083dfec72578ca526497335
varunpurohit76 <varunpurohit76@gmail.com>
2016-09-29 22:45:18 +0530

    added random stuff

--- monitor.sh
+++ monitor.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
-echo "----Memory usage----"
+echo "Mem(KiB)"
 MEM=$(top bn1 | grep "buff")
 echo "$MEM"
-echo "---"
+
 MEM_TOTAL=$(top bn1 | grep "buff" | cut -d':' -f 2 | cut -d',' -f 1 | tr -d '[[:blank:]]' | tr -d '[[:alpha:]]')
 echo "$MEM_TOTAL : total"
-echo "---"
+for ((c=0;c<50;c++))
+do
+	echo -n "="
+	sleep 0.02
+done
+echo ""
 
 MEM_FREE=$(top bn1 | grep "buff" | cut -d':' -f 2 | cut -d',' -f 2 | tr -d '[[:blank:]]' | tr -d '[[:alpha:]]')
 echo "$MEM_FREE : free"
-echo "---"
+EQ=$((50*$MEM_FREE/$MEM_TOTAL))
+for ((c=0;c<$EQ;c++))
+do
+	echo -n "="
+	sleep 0.02
+done
+echo ""
 
 MEM_USED=$(top bn1 | grep "buff" | cut -d':' -f 2 | cut -d',' -f 3 | tr -d '[[:blank:]]' | tr -d '[[:alpha:]]')
 echo "$MEM_USED : used"
-echo "---"
+EQ=$((50*$MEM_USED/$MEM_TOTAL))
+for ((c=0;c<$EQ;c++))
+do
+	echo -n "="
+	sleep 0.02
+done
+echo ""
 
-MEM_BUFF=$(top bn1 | grep "buff" | cut -d':' -f 2 | cut -d',' -f 4 | tr -d '[[:blank:]]' | tr -d '[[:alpha:]]')
-echo "$MEM_BUFF : buff cache"
-echo "---"
+MEM_BUFF=$(top bn1 | grep "buff" | cut -d':' -f 2 | cut -d',' -f 4 | tr -d '[[:blank:]]' | tr -d '[[:alpha:]]' | tr -d '[[:punct:]]')
+echo "$MEM_BUFF : buff/cache"
+EQ=$((50*$MEM_BUFF/$MEM_TOTAL))
+for ((c=0;c<$EQ;c++))
+do
+	echo -n "="
+	sleep 0.02
+done
+echo ""
+
+echo ""
+PING=$(ping -c 1 google.com)
+echo "Ping Test"
+if [ $? -eq 0 ];
+then
+	echo "Ping successful. Connected!"
+else
+	echo "Ping failed. Not Connected!"
+fi
